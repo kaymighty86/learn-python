32753c6d449b65091162e17f4ca97ec449588b4f
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-11-09 22:13:36 +0000

    Setting up Python Learning

---
+++ fundamentals/variables_and_types.py
@@ -0,0 +1,13 @@
+intNum = 23 #integer
+floatNum = 989.22 #float
+sumVariable = intNum + floatNum
+print (sumVariable) #show the output
+
+divVar = intNum / floatNum
+print(divVar)
+
+print(type(intNum)) #show the datatype 
+print(type(divVar)) #show the datatype
+
+print(intNum ** 2) # ** represent power arithmentic (e.g. 2^2 = 4). The same as Math.pow() function
+print(intNum // 13) # // represent power arithmentic. The same as Math.pow() function
